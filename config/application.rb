5786de74b06ac857abe5e022d6cb34b251f8b109
Mark Lohr <markuslohr@gmail.com>
2015-04-26 15:11:16 -0400

    deployment

--- config/application.rb
+++ config/application.rb
@@ -6,10 +6,6 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-# Enable the asset pipeline
-config.assets.enabled = true
-config.assets.paths << "#{Rails.root}/app/assets/fonts"
-
 module PatientList
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
